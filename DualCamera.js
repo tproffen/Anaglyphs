7c66ce403805b1c5f674e360bb7c1da2d9a13e1a
Thomas Proffen <thomasproffen@gmail.com>
2016-09-18 19:55:05 -0400

    Testing

--- DualCamera.js
+++ DualCamera.js
@@ -92,8 +92,8 @@ function start() {
     audio: {deviceId: audioSource ? {exact: audioSource} : undefined},
     video: {deviceId: videoSource ? {exact: videoSource} : undefined}
   };
-  navigator.getUserMedia(constraints).
-      then(gotStream).then(gotDevices).catch(handleError);
+//  navigator.mediaDevices.getUserMedia(constraints).
+//      then(gotStream).then(gotDevices).catch(handleError);
 }
 
 audioInputSelect.onchange = start;
