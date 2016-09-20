1da025a37edb76a6ab6fff2ae3d457b3a000cf74
Thomas Proffen <thomasproffen@gmail.com>
2016-09-19 20:59:26 -0400

    t

--- DualCamera.js
+++ DualCamera.js
@@ -19,14 +19,13 @@ function gotDevices(deviceInfos) {
   for (var i = 0; i !== deviceInfos.length; ++i) {
     var deviceInfo = deviceInfos[i];
     if (deviceInfo.kind === 'videoinput') {
+	  console.log('Video device: ', deviceInfo);
       var option = document.createElement('option');
 	  option.value = deviceInfo.deviceId;
       option.text = deviceInfo.label || 'camera ' + (videoSelect1.length + 1);
       videoSelect1.appendChild(option);
       option.text = deviceInfo.label || 'camera ' + (videoSelect2.length + 1);
       videoSelect2.appendChild(option);
-    } else {
-      console.log('Some other kind of source/device: ', deviceInfo);
     }
   }
   selectors.forEach(function(select, selectorIndex) {
