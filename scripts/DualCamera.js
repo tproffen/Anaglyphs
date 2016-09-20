8e0266306bb6774334cbf6085c6aae03252ec3d9
Thomas Proffen <thomasproffen@gmail.com>
2016-09-20 14:29:25 -0400

    Trying css filters

--- scripts/DualCamera.js
+++ scripts/DualCamera.js
@@ -93,3 +93,6 @@ videoSelect1.addEventListener("change", connectStream, false);
 videoSelect2.addEventListener("change", connectStream, false);
 snapButton.addEventListener("click", snapImage, false);
 
+video1.css="-webkit-filter: grayscale(100%);";
+
+
