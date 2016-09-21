4def30598cf193118d37311fde11f3c2eb046d49
Thomas Proffen <thomasproffen@gmail.com>
2016-09-21 12:09:23 -0400

    Update and rename anaglyphs.js to Anaglyphs.js

---
+++ scripts/Anaglyphs.js
@@ -0,0 +1,19 @@
+// Check if browser is Chrome - if not display warning
+
+function checkBrowser (divId) {
+
+var isChromium = window.chrome,
+    winNav = window.navigator,
+    vendorName = winNav.vendor,
+    isOpera = winNav.userAgent.indexOf("OPR") > -1,
+    isIEedge = winNav.userAgent.indexOf("Edge") > -1,
+    isIOSChrome = winNav.userAgent.match("CriOS");
+
+  if(isIOSChrome){
+     divID.innerHTML = 'These pages were developed for the Desktop version of Chrome and might not work on iOS.';
+  } else if(isChromium !== null && isChromium !== undefined && vendorName === "Google Inc." && isOpera == false && isIEedge == false) {
+     divID.innerHTML = '';
+  } else { 
+    divID.innerHTML = 'These pages were developed for the Desktop version of Chrome and might not work on other browsers.';
+  }
+}
