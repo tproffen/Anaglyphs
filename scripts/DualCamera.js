d94be04e53bbc70a26437f32574f4d34ecff3c06
Thomas Proffen <thomasproffen@gmail.com>
2016-09-21 10:15:11 -0400

    Update DualCamera.js

--- scripts/DualCamera.js
+++ scripts/DualCamera.js
@@ -102,4 +102,4 @@ navigator.mediaDevices.enumerateDevices().then(gotDevices).then(connectStream).c
 videoSelect1.addEventListener("change", connectStream, false);
 videoSelect2.addEventListener("change", connectStream, false);
 snapButton.addEventListener("click", snapImage, false);
-window.addEventListener("resize". determineSizes, false);
+window.addEventListener("resize", determineSizes, false);
