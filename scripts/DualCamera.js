5491136a969eae726bc473297f9cd26ee0e94f3e
Thomas Proffen <thomasproffen@gmail.com>
2016-10-21 14:55:50 -0400

    Removed brightness
    Too slow - implement in final image in the future

--- scripts/DualCamera.js
+++ scripts/DualCamera.js
@@ -7,7 +7,6 @@ var videoSelect2 = document.getElementById('videoSource2');
 var snapButton = document.getElementById('snap');
 var xOffset = document.getElementById('xOffset');
 var yOffset = document.getElementById('yOffset');
-var brightness = document.getElementById('brightness');
 var canvas = document.getElementById('canvas');
 var typeAna = document.getElementById('type_ana');
 var typeSbs = document.getElementById('type_sbs');
@@ -22,15 +21,8 @@ checkBrowser();
 var width=1280;
 var height=960;
 
-var contextLeft = canvasLeft.getContext('2d');
-var contextRight = canvasRight.getContext('2d');
-var context = canvas.getContext('2d');
-
 navigator.mediaDevices.getUserMedia({video:true});
 navigator.mediaDevices.enumerateDevices().then(gotDevices).then(readValues).then(connectStream).catch(handleError);
-
-//width=videoElement1.videoWidth;
-//height=videoElement1.videoHeight;
 
 canvas.height=height; 
 canvas.width=width;
@@ -39,8 +31,11 @@ canvasLeft.width=width;
 canvasRight.height=height; 
 canvasRight.width=width;
 
+var contextLeft = canvasLeft.getContext('2d');
+var contextRight = canvasRight.getContext('2d');
+var context = canvas.getContext('2d');
+
 determineSizes();
-drawWelcome();
 
 videoSelect1.addEventListener("change", connectStream, false);
 videoSelect2.addEventListener("change", connectStream, false);
@@ -48,18 +43,20 @@ videoSelect2.addEventListener("change", connectStream, false);
 snapButton.addEventListener("click", snapImage, false);
 xOffset.addEventListener("change", compositeImage, false);
 yOffset.addEventListener("change", compositeImage, false);
-brightness.addEventListener("change", compositeImage, false);
 typeAna.addEventListener("change", compositeImage, false);
 typeSbs.addEventListener("change", compositeImage, false);
 
 window.addEventListener("resize", determineSizes, false);
 window.addEventListener("unload", writeValues, false);
 
+drawWelcome();
+
 //================================================================================
 // Functions below
 //================================================================================
 
 function drawWelcome () {
+	
 	context.fillStyle = '#000000';
 	context.fillRect(0, 0, width, height);	
 
@@ -143,11 +140,9 @@ function compositeImage () {
 
 function compositeImageSbs () {
 	
-	var bright = brightness.valueAsNumber;
-	
 	context.fillStyle = '#000000';
 	context.fillRect(0, 0, width, height);
-	context.drawImage(canvasLeft, width/4, 0, width/2, height, 0,       0, width/2, height);
+	context.drawImage(canvasLeft,  width/4, 0, width/2, height, 0,       0, width/2, height);
 	context.drawImage(canvasRight, width/4, 0, width/2, height, width/2, 0, width/2, height);
 }
 
@@ -157,7 +152,6 @@ function compositeImageAna () {
 	
 	var offX= xOffset.valueAsNumber * width;
 	var offY= yOffset.valueAsNumber * height;
-	var bright = brightness.valueAsNumber;
 
 	var imageRight = contextRight.getImageData(0, 0,width, height);
 	var imageLeft = contextLeft.getImageData(0, 0, width, height);
@@ -167,30 +161,29 @@ function compositeImageAna () {
 	imageLeft = context.getImageData(0, 0, width, height);
 	
 	for (var i = 0; i < imageLeft.data.length; i += 4) {
- 		var brightLeft = wR * imageLeft.data[i] + wG * imageLeft.data[i + 1] + wB * imageLeft.data[i + 2];
+ 		var brightLeft  = wR * imageLeft.data[i]  + wG * imageLeft.data[i + 1]  + wB * imageLeft.data[i + 2];
  		var brightRight = wR * imageRight.data[i] + wG * imageRight.data[i + 1] + wB * imageRight.data[i + 2];
- 		imageRight.data[i]   = bright*brightLeft;  // Just swap red channel
- 		imageRight.data[i+1] = bright*brightRight; 
- 		imageRight.data[i+2] = imageRight.data[i+1];
+ 		imageRight.data[i]   = brightLeft;  // Just swap red channel
+ 		imageRight.data[i+1] = brightRight; 
+ 		imageRight.data[i+2] = brightRight;
     }	
 	
 	context.fillStyle = '#000000';
 	context.fillRect(0, 0, width, height);
 	context.putImageData(imageRight,-offX/2, -offY/2, offX, offY, width, height);
-
 }
 
 function determineSizes () {
 	
 	var padW=595;
-	var padH=120;
+	var padH=80;
 	
 	var newWidth=window.innerWidth-padW;
-	var newHeight=3*(newWidth/4);
+	var newHeight=3*(newWidth/4)+1;
 	
 	if (newHeight>(window.innerHeight-padH)) {
 		newHeight=window.innerHeight-padH;
-		newWidth=4*(newHeight/3);
+		newWidth=4*(newHeight/3)+1;
 	}
 	canvas.style.width=newWidth + 'px';
 	canvas.style.height=newHeight + 'px';
